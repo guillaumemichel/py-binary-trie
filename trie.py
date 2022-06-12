ba54607f532a336c5e2f3cddd613ebfab5fb6cab
Dennis Trautwein <git@dtrautwein.eu>
2022-06-12 08:42:55 +0200

    Add formatting, n_closest predicate and typings

--- trie.py
+++ trie.py
@@ -1,11 +1,14 @@
+from typing import TypeVar, List, Callable, Optional, Generic
 
-class Trie(object):
+T = TypeVar("T")
 
-    def __init__(self, metadata=None, key: str=""):
+
+class Trie(Generic[T]):
+    def __init__(self, metadata=Optional[T], key: str = ""):
         self.metadata = metadata
-        self.key = key # eg. "01110101"
-        self.branch = [None, None]
-        self.size = 0 # only counts the leaves
+        self.key = key  # eg. "01110101"
+        self.branch: List[Optional[Trie], Optional[Trie]] = [None, None]
+        self.size = 0  # only counts the leaves
 
     def __repr__(self) -> str:
         # "(branch[0].key / self.key \ branch[1].key)"
@@ -15,19 +18,19 @@ class Trie(object):
         rep += self.key
         if self.branch[1] is not None:
             rep += " \ " + self.branch[1].key
-        return rep+")"
-                
+        return rep + ")"
+
     # add the provided key to the trie
     # returns True on success and False on failure (usually
     # when the key is already in the trie)
-    def add(self, key: str, metadata=None) -> bool:
+    def add(self, key: str, metadata: Optional[T] = None) -> bool:
         if self.branch[int(key[len(self.key)])] is not None:
             # branch already exists
             branch = self.branch[int(key[len(self.key)])]
 
             minLen = min(len(key), len(branch.key))
             if key[:minLen] == branch.key[:minLen]:
-                if len(key)==len(branch.key):
+                if len(key) == len(branch.key):
                     # key already in the trie, cannot insert it
                     return False
                 else:
@@ -40,7 +43,7 @@ class Trie(object):
                     success = branch.add(key=key, metadata=metadata)
             else:
                 # insert between two nodes in the trie
-                # 
+                #
                 #   11 +insert(111001)     11 self
                 #  / \                    / \
                 #     \       -->          1110 mid
@@ -48,7 +51,7 @@ class Trie(object):
                 #     111011     key 111001 111011 branch
 
                 for i in range(minLen):
-                    if branch.key[i]!=key[i]:
+                    if branch.key[i] != key[i]:
                         # first bit where branch.key and key diverge
 
                         # create mid Trie node
@@ -56,12 +59,12 @@ class Trie(object):
                         # define mid branches
                         mid.branch[int(key[i])] = Trie(metadata=metadata, key=key)
                         mid.branch[int(key[i])].size = 1
-                        mid.branch[1-int(key[i])] = branch
+                        mid.branch[1 - int(key[i])] = branch
                         # set its size
                         mid.size = branch.size + 1
                         # update self branch to mid
                         self.branch[int(key[len(self.key)])] = mid
-                        success=True
+                        success = True
 
                         break
         else:
@@ -74,13 +77,13 @@ class Trie(object):
 
             self.branch[int(key[len(self.key)])] = Trie(metadata=metadata, key=key)
             self.branch[int(key[len(self.key)])].size = 1
-            success=True
+            success = True
 
         if success:
-            self.size+=1
+            self.size += 1
         return success
 
-    def find_trie(self, key:str):
+    def find_trie(self, key: str):
         if len(self.key) >= len(key):
             if self.key == key:
                 return self
@@ -90,7 +93,7 @@ class Trie(object):
             return self.branch[int(key[len(self.key)])].find_trie(key)
         else:
             return None
-                
+
     # returns metadata associated with the key, if the key is in the trie and has metadata
     # True if key in trie but no metadata and False otherwise
     def find(self, key: str) -> bool:
@@ -104,20 +107,22 @@ class Trie(object):
 
     # returns a list of the metadata of the n closest keys in the Trie to the given key
     # returns a list of keys if no metadata
-    def n_closest(self, key:str, n:int) -> list[str]:
+    def n_closest(self, key: str, n: int, predicate: Optional[Callable[[T], bool]] = None) -> List[str]:
         if self.branch[0] == self.branch[1] == None:
             # leaf of the trie
             if self.metadata is None:
                 return [self.key]
-            else:
+            elif predicate is None or predicate(self.metadata):
                 return [self.metadata]
-        
+            else:
+                return []
+
         nclosest = []
         if self.branch[int(key[len(self.key)])] is not None:
             # get n closest on the closest branch
-            nclosest += self.branch[int(key[len(self.key)])].n_closest(key, n)
-        if len(nclosest) < n and self.branch[1-int(key[len(self.key)])] is not None:    
+            nclosest += self.branch[int(key[len(self.key)])].n_closest(key, n, predicate=predicate)
+        if len(nclosest) < n and self.branch[1 - int(key[len(self.key)])] is not None:
             # if we don't have n keys yet, get the difference from the other branch
-            nclosest += self.branch[1-int(key[len(self.key)])].n_closest(key, n-len(nclosest))
-        return nclosest
+            nclosest += self.branch[1 - int(key[len(self.key)])].n_closest(key, n - len(nclosest), predicate=predicate)
 
+        return nclosest
